908021599ca5e531843b7f879da875c59820c3bf
kamaukimani <paulkimys285@gmail.com>
2026-09-22 16:00:56 +0300

    add a POST request to Newsletters class

--- app/routes/newsletter.py
+++ app/routes/newsletter.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource,Api 
 from flask import request,make_response 
 from app.models import Newsletter
+from app.db import db
 
 class Home(Resource):
     def get(self):
@@ -12,5 +13,22 @@ class Newsletters(Resource):
         response=make_response(
             newsletters,
             200
+        )
+        return response
+    def post(self):
+        data=request.get_json()
+        newsletter=Newsletter()
+        allowed_fields=["body","title"]
+        for attr,value in data.items():
+            if attr in allowed_fields:
+                setattr(newsletter,attr,value)
+        db.session.add(newsletter)
+        db.session.commit()
+
+        newsletter_dict=newsletter.to_dict()
+
+        response=make_response(
+            newsletter_dict,
+            201
         )
         return response
