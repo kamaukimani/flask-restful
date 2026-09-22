aaba26971845227538f66f41d2258d0d15c5a30a
kamaukimani <paulkimys285@gmail.com>
2026-09-22 15:51:45 +0300

    add Newsletters class to get all newsletters

--- app/routes/newsletter.py
+++ app/routes/newsletter.py
@@ -1,6 +1,16 @@
 from flask_restful import Resource,Api 
 from flask import request,make_response 
+from app.models import Newsletter
 
 class Home(Resource):
     def get(self):
         return {"message":"Welcome to the Newsletter RESTful API"},200
+
+class Newsletters(Resource):
+    def get(self):
+        newsletters=[newsletter.to_dict() for newsletter in Newsletter.query.all()]
+        response=make_response(
+            newsletters,
+            200
+        )
+        return response
